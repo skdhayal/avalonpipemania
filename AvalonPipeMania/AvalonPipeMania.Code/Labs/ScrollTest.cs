5e1baf5bea035528b25a8edface9dcddbd3681d1
dadeval <dadeval@dab8fd40-939d-11dd-96ca-7b7a724afd6b>
2008-10-10 17:44:11 +0000

    added smooth mouse scroll

--- AvalonPipeMania/AvalonPipeMania.Code/Labs/ScrollTest.cs
+++ AvalonPipeMania/AvalonPipeMania.Code/Labs/ScrollTest.cs
@@ -45,13 +45,26 @@ namespace AvalonPipeMania.Code.Labs
 
 			f.Tiles.Overlay.AttachTo(Overlay);
 
-			Overlay.MouseMove +=
-				(Sender, Arguments) =>
+			#region move the map with the mouse yet not too often anf smooth enough
+			Action<int, int> MoveTo = Tween.NumericEmitter.Of(
+				(x, y) =>
 				{
-					var p = Arguments.GetPosition(Overlay);
+					f.Tiles.Overlay.MoveTo(x, y);
+					f.Container.MoveTo(x, y);
+				}
+			);
+
+			Action<int, int> CalculateMoveTo = 
+				//Tween.NumericOmitter.Of(
+				(int_x, int_y) =>
+				{
+					double x = int_x;
+					double y = int_y;
+
+					//Console.WriteLine(new { x, y }.ToString());
 
 					const int PaddingX = Tile.Size / 2;
-					const int PaddingY = Tile.SurfaceHeight / 2;
+					const int PaddingY = Tile.SurfaceHeight;
 
 					const int MarginLeft = Tile.ShadowBorder + Tile.Size + PaddingX;
 					const int MarginWidth = MarginLeft + Tile.ShadowBorder + Tile.Size + PaddingX;
@@ -59,11 +72,23 @@ namespace AvalonPipeMania.Code.Labs
 					const int MarginTop = Tile.ShadowBorder + Tile.SurfaceHeight + PaddingY;
 					const int MarginHeight = MarginTop + Tile.ShadowBorder + Tile.Size + PaddingY;
 
-					var x = PaddingX + (DefaultWidth - (f.Tiles.Width + PaddingX * 2)) * ((p.X - MarginLeft).Max(0) / (DefaultWidth - MarginWidth)).Min(1);
-					var y = PaddingY + (DefaultHeight - (f.Tiles.Height + PaddingY * 2)) * ((p.Y - MarginTop).Max(0) / (DefaultHeight - MarginHeight)).Min(1);
+					var _x = PaddingX + (DefaultWidth - (f.Tiles.Width + PaddingX * 2)) * ((x - MarginLeft).Max(0) / (DefaultWidth - MarginWidth)).Min(1);
+					var _y = PaddingY + (DefaultHeight - (f.Tiles.Height + PaddingY * 2)) * ((y - MarginTop).Max(0) / (DefaultHeight - MarginHeight)).Min(1);
 
-					f.Tiles.Overlay.MoveTo(x, y);
-					f.Container.MoveTo(x, y);
+
+					MoveTo(Convert.ToInt32(_x), Convert.ToInt32(_y));
+				}
+			//)
+			;
+			#endregion
+
+
+			Overlay.MouseMove +=
+				(Sender, Arguments) =>
+				{
+					var p = Arguments.GetPosition(Overlay);
+
+					CalculateMoveTo(Convert.ToInt32(p.X), Convert.ToInt32(p.Y));
 				};
 
 			f.Tiles.Click +=
